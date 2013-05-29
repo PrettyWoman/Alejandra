36600aab762084277276611a1d460425b55ba47c
Alejandra <lachiky_1a@hotmail.com>
2013-05-29 14:13:15 -0600

    Commit Inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alejandra::Application.config.secret_token = '076b68c289c447b26c87f8dfc40c1b71461375fcf5d9357802824ab5ff91bc04e546f27afbf560efd37de9f10acfed1c7606941318c248a9aa5541b57bae3e3b'
