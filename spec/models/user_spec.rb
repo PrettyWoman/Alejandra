c402f271ac7b92893f32ed8980aa094b0ddb1d33
PrettyWoman <denisseaguilar1993@gmail.com>
2013-05-29 23:20:48 -0600

    capitulo 8 terminado

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,7 +25,8 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
-
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }  
   it { should be_valid }
   describe "when name is not present" do
     before { @user.name = " " }
@@ -35,7 +36,7 @@ describe User do
     before { @user.email = " " }
     it { should_not be_valid }
   end
-   describe "when name is too long" do
+  describe "when name is too long" do
     before { @user.name = "a" * 51 }
     it { should_not be_valid }
   end
@@ -99,5 +100,8 @@ describe User do
       specify { user_for_invalid_password.should be_false }
     end
   end
-
+ describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 end
